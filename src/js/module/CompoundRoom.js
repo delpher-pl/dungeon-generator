91ca75c52c077bf4985b9f6880cada5957f45358
delpher.pl <delpher00@wp.pl>
2019-06-29 19:29:05 +0200

    UPD js CompoundRoom: add addRoom()

--- src/js/module/CompoundRoom.js
+++ src/js/module/CompoundRoom.js
@@ -1,9 +1,13 @@
+import { randFunc } from "./util";
+import Room from "./Room";
+
 class CompoundRoom {
   constructor() {
     this.size = {
       x: 0,
       y: 0
     };
+    this.nextId = 1;
     this.tiles = [];
     this.rooms = [];
   }
@@ -26,6 +30,23 @@ class CompoundRoom {
 
   clearRooms() {
     this.rooms = [];
+  }
+
+  addRoom(rule) {
+    const randOffset = randFunc(rule.offsetMin, rule.offsetMax);
+    const randRoomSize = randFunc(rule.roomSizeMin, rule.roomSizeMax);
+
+    this.rooms.push({
+      id: this.nextId,
+      room: new Room(randRoomSize(), randRoomSize()).init(),
+      copyEmptyTiles: rule.copyEmptyTiles,
+      offset: {
+        x: randOffset(),
+        y: randOffset()
+      }
+    });
+
+    this.nextId += 1;
   }
 }
 
