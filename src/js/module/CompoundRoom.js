082e1846f3c1193768ba58bda34df47282ff5798
delpher.pl <delpher00@wp.pl>
2019-06-30 12:16:55 +0200

    UPD js CompoundRoom: add createRoom()

--- src/js/module/CompoundRoom.js
+++ src/js/module/CompoundRoom.js
@@ -48,6 +48,28 @@ class CompoundRoom {
 
     this.nextId += 1;
   }
+
+  createRoom(rules) {
+    let rulesArr;
+    if (Array.isArray(rules)) {
+      rulesArr = rules;
+    } else if (typeof rules === "object" && rules !== null) {
+      rulesArr = [rules];
+    } else if (rules) {
+      rulesArr = this.rules[rules];
+    } else {
+      throw Error("CompoundRoom.createRoom(rules): Invalid argument!");
+    }
+
+    for (let index = 0; index < rulesArr.length; index += 1) {
+      const o = Object.assign({}, this.rules.default, rulesArr[index]);
+      const { repeat } = o;
+
+      for (let r = 0; r < repeat; r += 1) {
+        this.addRoom(o);
+      }
+    }
+  }
 }
 
 module.exports = CompoundRoom;
