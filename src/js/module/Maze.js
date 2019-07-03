09a85bc37007f4fb5d3af5361b7c99646cc8eef2
delpher.pl <delpher00@wp.pl>
2019-07-03 18:40:50 +0200

    UPD js Maze: add getTileIndex(), getTile()

--- src/js/module/Maze.js
+++ src/js/module/Maze.js
@@ -10,6 +10,20 @@ class Maze {
   createEmptyMap() {
     this.tiles = Array.from({ length: this.size.x * this.size.y }, () => 0);
   }
+
+  getTileIndex(x, y) {
+    if (x >= 0 && x < this.size.x && y >= 0 && y < this.size.y) {
+      return this.size.x * y + x;
+    }
+    return -1;
+  }
+
+  getTile(x, y) {
+    if (x >= 0 && x < this.size.x && y >= 0 && y < this.size.y) {
+      return this.tiles[this.getTileIndex(x, y)];
+    }
+    return -1;
+  }
 }
 
 module.exports = Maze;
