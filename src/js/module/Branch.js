080b476dded2ad7870bc1146411bfad02ea53fe3
delpher.pl <delpher00@wp.pl>
2019-07-13 21:25:51 +0200

    UPD js Branch: add isInRange(), getTiles()

--- src/js/module/Branch.js
+++ src/js/module/Branch.js
@@ -2,6 +2,14 @@ class Branch {
   constructor() {
     this.tiles = [];
   }
+
+  isInRange(x, y) {
+    return x >= 0 && x < this.size.x && y >= 0 && y < this.size.y;
+  }
+
+  getTiles() {
+    return this.tiles;
+  }
 }
 
 module.exports = Branch;
