0287250981c8eb51b8551ac2b980204c912dc8ac
delpher.pl <delpher00@wp.pl>
2019-07-15 20:05:48 +0200

    UPD js Branch: add createEmptyBranch()

--- src/js/module/Branch.js
+++ src/js/module/Branch.js
@@ -7,6 +7,10 @@ class Branch {
     return x >= 0 && x < this.size.x && y >= 0 && y < this.size.y;
   }
 
+  createEmptyBranch() {
+    this.tiles = Array.from({ length: this.size.x * this.size.y }, () => 0);
+  }
+
   getTiles() {
     return this.tiles;
   }
